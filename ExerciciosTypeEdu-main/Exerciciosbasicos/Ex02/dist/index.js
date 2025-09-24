c7afeac2bbce5b144049b76767b959bb5e033cb9
anacarol-3 <ana9808101@gmail.com>
2025-09-23 22:34:49 -0300

    Update index.js

--- ExerciciosTypeEdu-main/Exerciciosbasicos/Ex02/dist/index.js
+++ ExerciciosTypeEdu-main/Exerciciosbasicos/Ex02/dist/index.js
@@ -4,8 +4,8 @@
  * Declare nome, idade e cidade e imprima uma frase.
  */
 Object.defineProperty(exports, "__esModule", { value: true });
-let nome = "Raul";
-let idade = 19;
+let nome = "Ana";
+let idade = 18;
 let cidade = "São Paulo";
 console.log(`Meu nome é ${nome}, tenho ${idade} anos e moro em ${cidade}.`);
 //# sourceMappingURL=index.js.map
